2d31a94ad2dfc4ca3bcb1281a296dafa233df833
gettingfunkier <dev.gettingfunkier@gmail.com>
2026-01-22 16:52:53 +0000

    add animation controller

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     
-    [SerializeField]
-    Rigidbody2D body;
+    [SerializeField] Rigidbody2D body;
+    [SerializeField] private Animator animator;
+
     public float moveSpeed = 5f;
     public float horizontalInput;
     public float terminalVelocity = -10;
+
+    bool isSprintingRight = false;
+    bool isSprintingLeft = false;
 
     void Start()
     {
@@ -18,15 +23,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        bool isMoving = Mathf.Abs(horizontalInput) > 0.1;
 
-        if (Mathf.Abs(horizontalInput) > 0.1)
+        if (isMoving)
         {
+            isSprintingLeft  = horizontalInput < 0f;
+            isSprintingRight = horizontalInput > 0f;
+
             body.linearVelocity = new Vector2(horizontalInput * moveSpeed, body.linearVelocity.y);
         }
         else
         {
+            isSprintingRight = false;
+            isSprintingLeft  = false;
+
             body.linearVelocity = new Vector2(0, body.linearVelocity.y);
         }
+
+        animator.SetBool("isSprintingRight", isSprintingRight);
+        animator.SetBool("isSprintingLeft", isSprintingLeft);
+
+        Debug.Log($"Input: {Input.GetAxis("Horizontal")}");
     }
 }
