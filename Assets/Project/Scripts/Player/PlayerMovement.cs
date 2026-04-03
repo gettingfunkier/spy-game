03c7c24a33258cce1d4e3fdc8db9ae5d42e4225b
gettingfunkier <dev.gettingfunkier@gmail.com>
2026-04-03 14:44:25 +0100

    Lookahead fixed! Turns out CheckMovementDirection() wasn't being called. For future context, I added this function in addition to CheckSprintDirection(), because the latter takes into account if the character is jumping. And I still want lookahead when Lily jumps :

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -64,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         CheckLookDirection();
+        CheckMovementDirection();
         CheckSprintDirection();
         CheckTerminalVelocity();
         RareIdleAnimation();
