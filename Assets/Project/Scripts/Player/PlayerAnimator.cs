26e264489396260a981176070ada369ed73bc9f5
gettingfunkier <dev.gettingfunkier@gmail.com>
2026-04-04 11:26:07 +0100

    added idle to sprint transition back, now scripted! (TM)

--- Assets/Project/Scripts/Player/PlayerAnimator.cs
+++ Assets/Project/Scripts/Player/PlayerAnimator.cs
@@ -15,10 +15,17 @@ public class PlayerAnimator : MonoBehaviour
     {
         spriteRenderer.flipX = movement.isLookingLeft;
 
+        if (IsPlayingTransitionAnimation()) return;
+        // if transition animation is playing, don't interrupt it!
+
         if (!movement.isGrounded)
         {
             // animator.Play("Lily_Jump");
             // animator.Play("Lily_Fall");
+        }
+        else if (movement.isSprinting && IsPlayingIdleAnimation())
+        {
+            animator.Play("Lily_SprintTransition");
         }
         else if (movement.isSprinting)
         {
@@ -32,10 +39,29 @@ public class PlayerAnimator : MonoBehaviour
         }
     }
 
+    bool IsPlayingIdleAnimation()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Lily_Idle") 
+            || animator.GetCurrentAnimatorStateInfo(0).IsName("Lily_CheckEarpiece"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool IsPlayingTransitionAnimation()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Lily_SprintTransition")
+            && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+        {
+            return true;
+        }
+        return false;
+    }
+
     bool RareIdleAnimation()
     {
         int randomValue = Random.Range(0, 5000);
-        // Debug.Log(randomValue);
         if (randomValue <= 2 && !movement.isMoving && movement.isGrounded)
         {
             animator.Play("Lily_CheckEarpiece");
